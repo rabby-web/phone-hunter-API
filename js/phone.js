b6198eb2c6d6293f188bd0f0a79e6c3a28649e85
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-28 17:05:15 +0600

    t dainamik data load phone hunter

--- js/phone.js
+++ js/phone.js
@@ -4,6 +4,34 @@ const loadPhone = async () => {
   );
   const data = await res.json();
   const phones = data.data;
-  console.log(phones);
+  // console.log(phones);
+  displayPhones(phones);
 };
+const displayPhones = (phones) => {
+  // console.log(phones);
+  const phoneContainer = document.getElementById("phone-container");
+  phones.forEach((phone) => {
+    console.log(phone);
+    // 1 create a div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card card-compact w-96 bg-gray-300 shadow-xl`;
+    phoneCard.innerHTML = `
+      <figure>
+        <img
+          src="${phone.image}"
+          alt="Shoes"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    `;
+    phoneContainer.appendChild(phoneCard);
+  });
+};
+
 loadPhone();
